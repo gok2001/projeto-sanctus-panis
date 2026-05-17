7fea064c06bdd9fcb2317e01812ebb469f37100d
Guilherme <guilhermeokern@gmail.com>
2026-05-17 14:34:06 -0300

    Ajuste no estilo cardápio

--- front-end/scripts/cardapio.js
+++ front-end/scripts/cardapio.js
@@ -68,6 +68,7 @@ function criarIngrediente() {
         <select>
         <input type="number" class="quantidade-input" placeholder="Quantidade">
         <input type="text" class="unidade-input" placeholder="g/un">
+        <button type="button" class="btn btn-remove">X</button>
     `;
 
     return div;
