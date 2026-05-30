afd13085acadf3838c8bb6bd6fa3f6a1ddb02c45
Guilherme <guilhermeokern@gmail.com>
2026-05-30 18:33:47 -0300

    Adiciona finalização de pedido

--- front-end/scripts/pagamento.js
+++ front-end/scripts/pagamento.js
@@ -1,9 +1,16 @@
+const API_URL = 'http://localhost:1880';
+const URL_addPedido = `${API_URL}/addPedido`;
+const URL_addItemPedido = `${API_URL}/addItemPedido`;
+
+const formFinalizarPedido = document.querySelector('.form-finalizar-pedido');
 const itensResumo = document.querySelector('#itens-resumo');
 const totalResumo = document.querySelector('#total-resumo');
 const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
 const selectPagamento = document.querySelector('#seleciona-pagamento');
 const trocoContainer = document.querySelector('#troco-container');
 const inputTroco = document.querySelector('#troco');
+
+const usuario = JSON.parse(localStorage.getItem('usuarioLogado'));
 
 window.addEventListener('DOMContentLoaded', () => {
     carregarResumoPedido();
@@ -16,6 +23,72 @@ window.addEventListener('DOMContentLoaded', () => {
 
     selectPagamento.addEventListener('change', handlePagamentoChange);
 });
+
+formFinalizarPedido.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    if (!usuario) {
+        alert('Você precisa estar logado!');
+        return;
+    }
+
+    if (carrinho.length === 0) {
+        alert('Carrinho vazio');
+        return;
+    }
+
+    try {
+        const responsePedido = await fetch(
+            URL_addPedido,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({idUsuario: usuario.idUsuario})
+            }
+        );
+
+        if (!responsePedido.ok) {
+            throw new Error('Erro ao criar pedido');
+        }
+
+        const pedidoCriado = await responsePedido.json();
+        const idPedido = pedidoCriado.insertId;
+        
+        for (const item of carrinho) {
+            const dadosItem = {
+                idPedido,
+                idLanche: item.id,
+                quantidade: item.quantidade
+            };
+
+            const responseItem = await fetch(
+                URL_addItemPedido,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(dadosItem)
+                }
+            );
+
+            if (!responseItem.ok) {
+                throw new Error('Erro ao adicionar item');
+            }
+        }
+
+        localStorage.removeItem('carrinho');
+
+        alert('Pedido realizado com sucesso!');
+
+        window.location.href = './index.html';
+    } catch (erro) {
+        console.error(erro);
+        alert('Erro ao finalizar o pedido');
+    }
+})
 
 function carregarResumoPedido() {
     itensResumo.innerHTML = '';
