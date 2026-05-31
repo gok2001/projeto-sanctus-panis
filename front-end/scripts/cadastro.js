ead337d14a04b7106a57ea3c22d974b23b851e36
Guilherme <guilhermeokern@gmail.com>
2026-05-31 16:19:43 -0300

    Adiciona validações no cadastro

--- front-end/scripts/cadastro.js
+++ front-end/scripts/cadastro.js
@@ -10,39 +10,74 @@ formCadastro.addEventListener('submit', async (event) => {
     btnCadastro.disabled = true;
     btnCadastro.textContent = 'Cadastrando...';
 
-    const nomeUsuario = document.querySelector('#nome-cadastro').value;
-    const emailUsuario = document.querySelector('#email-cadastro').value;
-    const cpfUsuario = document.querySelector('#cpf').value;
+    const nomeUsuario = document.querySelector('#nome-cadastro').value.trim();
+    const emailUsuario = document.querySelector('#email-cadastro').value.trim();
+    const cpfUsuario = document.querySelector('#cpf').value.trim;
     const senhaUsuario = document.querySelector('#senha-cadastro').value;
     const confirmarSenha = document.querySelector('#confirmar-senha').value;
 
-    if (senhaUsuario !== confirmarSenha) {
-        alert('As senhas não coincidem!');
-        return;
-    }
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    const dados = {
-        nomeUsuario,
-        emailUsuario,
-        cpfUsuario,
-        senhaUsuario
-    };
 
     try {
-        await fetch(
+        if (!nomeUsuario) {
+            alert('Informe seu nome');
+            return;
+        }
+
+        if (!emailUsuario) {
+            alert('Informe seu email');
+            return;
+        }
+
+        if (!regexEmail.test(emailUsuario)) {
+            alert('Email inválido');
+            return;
+        }
+
+        if (!cpfUsuario) {
+            alert('Informe seu CPF');
+            return;
+        }
+
+        if (senhaUsuario.length < 8) {
+            alert('A senha deve possuir pelo menos 8 caracteres');
+            return;
+        }
+
+        if (senhaUsuario !== confirmarSenha) {
+            alert('As senhas não coincidem!');
+            return;
+        }
+
+        const novoUsuario = {
+            nomeUsuario,
+            emailUsuario,
+            cpfUsuario,
+            senhaUsuario
+        };
+
+        const response = await fetch(
             `${API_URL}/addUsuario`,
             {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(dados)
+            body: JSON.stringify(novoUsuario)
         });
+
+        if (!response.ok) {
+            throw new Error('Erro ao cadastrar usuário');
+        }
+
         alert('Cadastro enviado!');
         window.location.href = 'login.html';
+
     } catch (erro) {
         console.error('Erro:', erro);
         alert('Erro ao cadastrar usuário');
+        
     } finally {
         btnCadastro.disabled = false;
         btnCadastro.textContent = 'Cadastrar';
